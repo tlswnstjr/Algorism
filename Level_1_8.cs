ad30b77b580d898db6643afe255f3b195961398a
tlswnstjr78@naver.com <tlswnstjr78@naver.com>
2021-04-29 17:16:02 +0900

    04-29

---
+++ Level_1_8.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_1_8 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] a = new int[] { 4, 7, 12 };
+        bool[] s = new bool[] { true, false, true };
+        Debug.Log(solution(a, s));
+    }
+
+    public int solution(int[] absolutes, bool[] signs)
+    {
+        int answer = 0;
+        for (int i = 0; i < signs.Length; i++)
+        {
+            if(signs[i] == true)
+            {
+                answer += absolutes[i]; 
+            }
+            else
+            {
+                answer += -absolutes[i];
+
+            }
+        }
+
+        return answer;
+    }
+}
