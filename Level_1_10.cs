308140ff31b33805372e1e5f5db238574c226f42
tlswnstjr78@naver.com <tlswnstjr78@naver.com>
2021-05-02 12:49:00 +0900

    05-02
    알고리즘 문제 풀이

---
+++ Level_1_10.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_1_10 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(solution("Zbcdefg"));
+    }
+
+    public char[] _ans;
+    public string solution(string s)
+    {
+        string answer = "";
+
+        char[] _ans = new char[s.Length];
+
+        for (int i = 0; i < _ans.Length; i++)
+        {
+            _ans[i] = s[i];
+        }
+        for (int i = 0; i < _ans.Length; i++)
+        {
+            for (int j = i+1; j < _ans.Length; j++)
+            {
+                char ch = ' ';
+                if(_ans[i] < _ans[j])
+                {
+                    ch = _ans[i];
+                    _ans[i] = _ans[j];
+                    _ans[j] = ch;
+                }          
+            }
+            answer += _ans[i];
+        }
+
+
+        return answer;
+    }
+}
---
+++ Level_1_9.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Level_1_9 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] a = new string[] { "abcd", "abce", "cdx" };
+        string[] s = new string[] { };
+
+        s = solution(a, 2);
+        for (int i = 0; i < a.Length; i++)
+        {
+            Debug.Log(i+" >>>>>>>>>>>>>>>> " +s[i]);
+           
+        }
+    }
+
+    //strings는 글자 배열을 받아 올 인자입니다
+    //n보다 배열의 크기가 작을 순 없습니다 
+    public string[] solution(string[] strings, int n)
+    {
+        string[] answer = new string[] { };
+        for (int i = 0; i < strings.Length; i++)
+        {
+            for (int j = i+1; j < strings.Length; j++)
+            {
+                char a = strings[i][n];
+                char b = strings[j][n];
+
+                if(b<a)
+                {
+                    string h = strings[j];
+                    strings[j] = strings[i];
+                    strings[i] = h;
+                }
+                else if(b == a)
+                {
+                    string[] _str = new string[2];
+
+                    _str[0] = strings[i];
+                    _str[1] = strings[j];
+
+                    Array.Sort(_str);
+                    strings[i] = _str[0];
+                    strings[j] = _str[1];
+
+                }
+            }
+            //Debug.Log("인덱스 글자입니다" + a);
+        }
+
+        //Array.Sort(strings);
+        answer = strings;
+
+        return answer;
+    }
+
+}
